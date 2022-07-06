7ef51b96088615609a0355c6c7ca51e5f4f3035e
Carlos Mestre <CARMESCE@gmail.com>
2022-07-06 03:34:55 +0200

    Ahorcado

---
+++ README.md
@@ -0,0 +1,34 @@
+### Ahorcado (2pts)
+
+Forquea y clona el programa que encontrarás en ```https:github.com/elMestreAcademy/ahorcado```
+
+El programa no contabiliza el numero de intentos. asegurate que cada vez que se falla una letra se decrementa el numero de intentos
+Verás que hay una serie de dibujos en el fichero ```hangmanpics.py```. Muestra la imagen correspondiente asociada al numero de intentos restantes después de cada jugada. Muestra también la imgen ```SAVED``` si se descubre la palabra.
+El diccionario de palabras es sumamente escaso. Escoge la palabra al azar a partir del fichero ```palabras.txt``
+
+Por favor, ejecuta todas las modificaciones y arregla todos problemas, cada punto con su commit pertinente.
+Indica el repo donde se puede encontrar la solución (asegurate de que sea público)
+Anota en el papel los cambios aplicados ```(git diff)```
+
+- el núm. de línea o líneas donde se encuentra el error
+- el código actual
+- la solución propuesta
+- respeta esta convención en el resto de preguntas
+
+ex:
+
+<http://github.com/usuario/repositorio>
+
+```diff
+diff --git a/example.py b/example.py
+index 73e32f3..af7b241 100644
+--- a/example.py
++++ b/example.py
+@@ -100,7 +100,7 @@ class Foo:
+ foo = 0
+ bar = 0
+- print "hell oworld"
++ print("hello world")
+
+ foo += 1
+```
---
+++ main.py
@@ -0,0 +1,83 @@
+import random
+import re
+
+MAX_GUESS = 6
+
+
+def remove_tilts(old):
+    new = old.lower()
+    new = re.sub(r'[àáâãäå]', 'a', new)
+    new = re.sub(r'[èéêë]', 'e', new)
+    new = re.sub(r'[ìíîï]', 'i', new)
+    new = re.sub(r'[òóôõö]', 'o', new)
+    new = re.sub(r'[ùúûü]', 'u', new)
+
+    return new
+
+
+def pick_random_word():
+    words = [
+        'pelota',
+        'chancho',
+        'diccionario'
+    ]
+
+    index = random.randint(0, len(words) - 1)
+
+    word = remove_tilts(words[index].strip()).upper()
+
+    return word
+
+
+def ask_user_for_next_letter():
+    letter = input("Guess your letter: ")
+    return letter.strip().upper()
+
+
+def generate_word_string(word, letters_guessed):
+    output = []
+    for letter in word:
+        if letter in letters_guessed:
+            output.append(letter.upper())
+        else:
+            output.append("_")
+    return " ".join(output)
+
+
+def main():
+    WORD = pick_random_word()
+
+    letters_to_guess = set(WORD)
+    correct_letters_guessed = set()
+    incorrect_letters_guessed = set()
+    num_guesses = 0
+
+    print("Bienvenid@ al jeugo del ahorcado!")
+    while (len(letters_to_guess) > 0) and num_guesses < MAX_GUESS:
+        guess = ask_user_for_next_letter()
+
+        if guess in correct_letters_guessed or guess in incorrect_letters_guessed:
+            print("Ya has escogido esa letra.")
+            continue
+
+        if guess in letters_to_guess:
+            letters_to_guess.remove(guess)
+            correct_letters_guessed.add(guess)
+        else:
+            incorrect_letters_guessed.add(guess)
+
+        word_string = generate_word_string(WORD, correct_letters_guessed)
+
+        print(f"""{word_string}""")
+
+        print("Te quedan {} intentos".format(MAX_GUESS - num_guesses))
+
+    # tell the user they have won or lost
+    if num_guesses < MAX_GUESS:
+        print("Felicidades, has acertado correctamente la palabra: {}".format(WORD))
+    else:
+        print("Lo Siento!, la palabra que buscanas era: {}".format(WORD))
+
+
+if __name__ == '__main__':
+    main()
